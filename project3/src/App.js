5f6f0d76a313094b130a9eae717d44258762fd89
Stephen Rogers <stephenrogers@iMac-1548.local>
2019-09-05 15:36:48 -0400

    added pages folder, with home.js

--- project3/src/App.js
+++ project3/src/App.js
@@ -11,7 +11,7 @@ function App() {
     //       <Wrapper>
     //         <Route exact path="/" component={Home} />
     //         <Route exact path="/about" component={About} />
-    //         <Route exact path="discover" component={Discover} />
+    //         <Route exact path="/discover" component={Discover} />
     //         <Route exact path="/search" component={Search} />
     //       </Wrapper>
     //     <Footer />
