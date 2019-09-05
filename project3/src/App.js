37438270ec094e526fa6554de2ff2ffe1cf02635
Stephen Rogers <stephenrogers@iMac-1548.local>
2019-09-05 14:51:49 -0400

    centered navbar, added icon

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar"
+import Discover from '../../../UNCCCLT201904FSF2/20-react/01-Activities/06-Stu_PupsterApp/reactpractice/src/pages/Discover';
 
 function App() {
   return (
-    <>
-      <Navbar></Navbar>
-    </>
+    // <Router>
+    //   <>
+        <Navbar />
+    //       <Wrapper>
+    //         <Route exact path="/" component={Home} />
+    //         <Route exact path="/about" component={About} />
+    //         <Route exact path="discover" component={Discover} />
+    //         <Route exact path="/search" component={Search} />
+    //       </Wrapper>
+    //     <Footer />
+    //   </>
+    // </Router>
   );
 }
 
--- project3/src/components/Navbar/index.js
+++ project3/src/components/Navbar/index.js
@@ -1,9 +1,33 @@
 import React from "react";
 import "./style.css";
+import logo from "./logo.png"
+
 
 function Navbar() {
     return (
-        <p>Navbar Placeholder</p>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <img src={logo} style={{width: "45px", height: "50px"}} className="navbar-brand" href="#"></img>
+            <a className="navbar brand">Paint A Good Boy</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav mx-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Features</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Pricing</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
