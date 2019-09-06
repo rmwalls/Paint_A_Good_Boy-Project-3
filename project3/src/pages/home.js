5d80fcfeb65c9ce26595b18584ad2ddc7fb9b2bb
Stephen Rogers <stephenrogers@iMac-1548.local>
2019-09-06 08:01:01 -0400

    added text on homepage

--- project3/src/pages/home.js
+++ project3/src/pages/home.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Hero from "../components/Hero";
-// import Container from "../components/Container";
-// import Row from "../components/Row";
-// import Col from "../components/Col";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
 
 function Home() {
     return (
         <>
           <Hero backgroundImage="https://data.1freewallpapers.com/download/autumn-animals-leaves-grass-dogs-puppies-adventure-golden-retriever-fallen-high-quality.jpg">
-          
+            <Container>
+              <Row>
+                <Col size="md-6">
+                  <h1 className="display-1 text-white">Give Them </h1>
+                </Col>
+              </Row>
+            </Container>
           </Hero>
         </>
     )
