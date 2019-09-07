b6d385c97b7bb8508bce805aa02210de402144eb
ZelleM <groovyrlm@gmail.com>
2019-09-07 00:01:34 -0400

    User Registration db model and component; deleted React logo/favicons

---
+++ models/userRegModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  fullname: {
+    type: String,
+    trim: true,
+    required: "Full Name is Required"
+  },
+  username: {
+    type: String,
+    trim: true,
+    required: "Username is Required"
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [
+      function(input) {
+        return input.length >= 6;
+      },
+      "Password should be longer."
+    ]
+  },
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+  },
+  userCreated: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var User = mongoose.model("User", UserSchema);
+
+// Export the User model
+module.exports = User;
