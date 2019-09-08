96bd398057af98f87fdf490b92eed9306828e424
ZelleM <groovyrlm@gmail.com>
2019-09-08 18:42:36 -0400

    straightned folder structure

--- client/src/components/Col/index.js
+++ client/src/components/Col/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-function Row(props) {
-    return <div className={`row${props.fluid ? "-fluid" : ""}`} {...props} />
+function Col(props) {
+  const size = props.size.split(" ").map(size => "col-" + size).join(" ");
+
+  return <div className={size} {...props} />;
 }
+
+export default Col;
