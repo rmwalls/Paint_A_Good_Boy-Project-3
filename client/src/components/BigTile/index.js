20718863ff50da45793aa56bdacfefa2027b89aa
Stephen Rogers <stephenrogers@iMac-1548.local>
2019-09-11 15:48:27 -0400

    added some placeholder images

---
+++ client/src/components/BigTile/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function BigTile() {
+    return (
+        <>
+            <a href="https://placeholder.com"><img src="https://via.placeholder.com/800" /></a>
+        </>
+    )
+}
+
+export default BigTile;
