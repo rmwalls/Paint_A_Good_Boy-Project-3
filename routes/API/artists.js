b8b177199ed27b84d6a8e85fb39867428661dbac
mwmcguire <4903435649034356+mwmcguire@users.noreply.github.com>
2019-09-12 17:28:12 -0400

    get route for artist info

--- routes/API/artists.js
+++ routes/API/artists.js
@@ -3,4 +3,17 @@ const router = express.Router();
 
 const Artist = require('../../models/Artist');
 
+// @route    GET api/artists
+// @desc     Get artist info
+// @access   Public
+router.get('/', (req, res) => {
+  Artist.find({})
+    .then(artist => {
+      res.json(artist);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 module.exports = router;
