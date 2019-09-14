99b2bd7ed94f189c5619d7b0ff1eea8b71633b80
mwmcguire <4903435649034356+mwmcguire@users.noreply.github.com>
2019-09-13 22:14:31 -0400

    Prepared for deployment

--- routes/API/artists.js
+++ routes/API/artists.js
@@ -16,6 +16,19 @@ router.get('/', (req, res) => {
     });
 });
 
+// @route    GET api/artists/photographers
+// @desc     Get artist info by photographer
+// @access   Public
+router.get('/', (req, res) => {
+  Artist.find({})
+    .then(artist => {
+      res.json(artist);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 // @route    POST api/artists
 // @desc     Create a new artist
 // @access   Public
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 // const routes = require('./routes');
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,20 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
-
-// API Test Route
-app.get('/', (req, res) => res.send('Project 3 API Running'));
-
-// Routes
-app.use('/api/artists', require('./routes/api/artists.js'));
-
-// app.use(routes);
-
 // Connect to the Mongo DB
-// console.log(process.env.MONGODB_URI);
 const url = process.env.MONGODB_URI || 'mongodb://localhost/petapp';
 mongoose.connect(url, {
   useNewUrlParser: true,
@@ -38,6 +26,22 @@ db.once('open', _ => {
 db.on('error', err => {
   console.error('connection error:', err);
 });
+
+// Routes
+app.use('/api/artists', require('./routes/api/artists.js'));
+app.use('/api/users', require('./routes/api/users.js'));
+
+// app.use(routes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Start the API server
 app.listen(PORT, function() {
